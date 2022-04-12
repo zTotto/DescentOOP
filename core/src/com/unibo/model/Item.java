49e4aed3ba9d1277693ad9e1dc968d9dfdc7c53b
Lorenzo Todisco <lorenzo.todisco@studio.unibo.it>
2022-04-12 13:59:32 +0200

    Added abstract item class

---
+++ core/src/com/unibo/model/Item.java
@@ -0,0 +1,83 @@
+package com.unibo.model;
+
+import java.util.Objects;
+
+/**
+ * 
+ * Basic class modeling a generic Item.
+ */
+public abstract class Item {
+
+    private final String name;
+    private final String id;
+    private Position pos = new Position();
+
+    /**
+     * Constructor to build an item.
+     * 
+     * @param name The item name.
+     * @param id   The item ID.
+     */
+    public Item(final String name, final String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    /**
+     * 
+     * @return the item name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @return the item ID.
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * @return the item position.
+     */
+    public Position getPos() {
+        return new Position(pos.getxCoord(), pos.getyCoord());
+    }
+
+    /**
+     * Sets the item position to the specified position.
+     * 
+     * @param p
+     */
+    public void setPos(final Position p) {
+        this.pos.setxCoord(p.getxCoord());
+        this.pos.setyCoord(p.getyCoord());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
