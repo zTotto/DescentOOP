b20ea9410c147d9f43f26829e18c3f43112fe7e7
Lorenzo Todisco <lorenzo.todisco@studio.unibo.it>
2022-04-12 14:04:59 +0200

    Added Hero class

---
+++ core/src/com/unibo/model/Hero.java
@@ -0,0 +1,31 @@
+package com.unibo.model;
+
+/**
+ * 
+ * A class to model the Hero.
+ */
+@SuppressWarnings("PMD.MissingSerialVersionUID")
+public class Hero extends Character {
+
+    private final String name;
+
+    /**
+     * Constructor for the Hero.
+     * @param name
+     * @param maxHp
+     * @param speed
+     * @param startingWeapon
+     */
+    public Hero(final String name, final int maxHp, final int speed, final Weapon startingWeapon) {
+        super(maxHp, speed, startingWeapon);
+        this.name = name;
+    }
+
+    /**
+     * 
+     * @return the Hero's name
+     */
+    public String getName() {
+        return name;
+    }
+}
