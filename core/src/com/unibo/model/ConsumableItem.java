fb6438f7d90bc2988afe4115222b5f429a71cffa
Lorenzo Todisco <lorenzo.todisco@studio.unibo.it>
2022-04-12 14:03:19 +0200

    Added abstract ConsumableItem class

---
+++ core/src/com/unibo/model/ConsumableItem.java
@@ -0,0 +1,35 @@
+package com.unibo.model;
+
+/**
+ * 
+ * Class to model a consumable, like a health potion or damage buff potion.
+ */
+public abstract class ConsumableItem extends Item{
+
+    private final double modifier;
+
+    /**
+     * Constructor for a consumable item.
+     * @param name of the item
+     * @param id of the item
+     * @param mod to apply to the user of the item
+     */
+    public ConsumableItem(final String name, final String id, final double mod) {
+        super(name, id);
+        this.modifier = mod;
+    }
+
+    /**
+     * 
+     * @return the value of the consumable (for example replenished Hp).
+     */
+    public double getModifier() {
+        return modifier;
+    }
+
+    /**
+     * Uses the consumable on the selected pg.
+     * @param pg the character that gets the buff.
+     */
+    public abstract void use(Character pg);
+}
