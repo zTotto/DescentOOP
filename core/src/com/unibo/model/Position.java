7d07dc012a80ac819016108c7029c4dc42b644dd
Lorenzo Todisco <lorenzo.todisco@studio.unibo.it>
2022-04-12 14:01:22 +0200

    Added position class (items and characters)

---
+++ core/src/com/unibo/model/Position.java
@@ -0,0 +1,87 @@
+package com.unibo.model;
+
+import java.util.Objects;
+
+/**
+ * 
+ * A class to locate in space a certain entity.
+ */
+public class Position {
+
+    private int x;
+    private int y;
+
+    public Position() {
+        //Empty Constructor
+    }
+
+    /**
+     * Constructor with x and y coordinates.
+     * @param x coordinate
+     * @param y coordinate
+     */
+    public Position(final int x, final int y) {
+        this.setxCoord(x);
+        this.setyCoord(y);
+    }
+
+    /**
+     * 
+     * @return the y coordinate
+     */
+    public int getyCoord() {
+        return y;
+    }
+
+    /**
+     * 
+     * @return the x coordinate
+     */
+    public int getxCoord() {
+        return x;
+    }
+
+    /**
+     * Sets the y coordinate to the specified one.
+     * @param yCoord
+     */
+    public void setyCoord(final int yCoord) {
+        this.y = yCoord;
+    }
+
+    /**
+     * Sets the x coordinate to the specified one.
+     * @param xCoord
+     */
+    public void setxCoord(final int xCoord) {
+        this.x = xCoord;
+    }
+
+    /**
+     * 
+     * @return the position
+     */
+    public Position getPosition() {
+        return new Position(x, y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final Position other = (Position) obj;
+        return x == other.x && y == other.y;
+    }
+    public String toString() {
+        return "X: " + x + ", Y: " + y;
+    }
+}
