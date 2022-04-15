32e1c30751c82adc6cc1f071772b8c1dd6323d95
Lorenzo Todisco <lorenzo.todisco@studio.unibo.it>
2022-04-15 13:50:09 +0200

    Added game icon

--- desktop/src/com/unibo/game/DesktopLauncher.java
+++ desktop/src/com/unibo/game/DesktopLauncher.java
@@ -5,12 +5,13 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("Descent");
-	    config.setWindowedMode(Descent.GAME_WIDTH, Descent.GAME_HEIGHT);
-	    config.useVsync(true);
-	    config.setForegroundFPS(165);
-		new Lwjgl3Application(new Descent(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Descent");
+        config.setWindowedMode(Descent.GAME_WIDTH, Descent.GAME_HEIGHT);
+        config.useVsync(true);
+        config.setForegroundFPS(165);
+        config.setWindowIcon("pngegg.png");
+        new Lwjgl3Application(new Descent(), config);
+    }
 }
