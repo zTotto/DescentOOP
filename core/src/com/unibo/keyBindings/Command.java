aef14ccc518f0856fa73d3c535a94f497ebcc434
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2022-05-04 19:49:57 +0200

    Add javadoc

--- core/src/com/unibo/keyBindings/Command.java
+++ core/src/com/unibo/keyBindings/Command.java
@@ -2,7 +2,16 @@ package com.unibo.keyBindings;
 
 import com.unibo.view.CharacterView;
 
+/**
+ * Interface of a Command
+ *
+ */
 public interface Command {
 
+	/**
+	 * Execute a command on a specific character
+	 * 
+	 * @param character the character on which the command will be executed
+	 */
 	void Execute(CharacterView character);
 }
--- core/src/com/unibo/keyBindings/InputHandler.java
+++ core/src/com/unibo/keyBindings/InputHandler.java
@@ -6,9 +6,26 @@ import java.util.Optional;
 
 import com.badlogic.gdx.Gdx;
 
+/**
+ * Class that handle the input, in order to give a Command object, 
+ * referred to the action the player want to execute (based on what he pressed)
+ *
+ */
 public class InputHandler {
 	private final Map<String, Command> commands = new HashMap<>();
 	
+	/**
+	 * Constructor of the InputHandler
+	 * 
+	 * @param attack command that execute the attack action
+	 * @param pickUp command that execute the pick up action
+	 * @param switchWeapon command that execute the weapon's switch action
+	 * @param moveUp command that move up a character
+	 * @param moveRight command that move right a character
+	 * @param moveDown command that move down a character
+	 * @param moveLeft command that move left a character
+	 * @param pause command that pause the game
+	 */
 	public InputHandler(Command attack, Command pickUp, Command switchWeapon, Movement moveUp, Movement moveRight, Movement moveDown, Movement moveLeft, Command pause) {
 		this.commands.put(KeyBindings.ATTACK.getName(), attack);
 		this.commands.put(KeyBindings.PICK_UP.getName(), pickUp);
@@ -20,6 +37,10 @@ public class InputHandler {
 		this.commands.put(KeyBindings.PAUSE.getName(), pause);
 	}
 	
+	/**
+	 * @param key the key that the user pressed (or better, the action the user want to perform)
+	 * @return an optional of the command related to the action the user want do perform
+	 */
 	public Optional<Command> handleInput(KeyBindings key) {
 		
 		if(KeyBindings.MOVE_DOWN == key 
