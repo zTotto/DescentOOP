7faac66f72897080449ef0647eee6077284af4e0
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2022-05-15 13:34:00 +0200

    Add javadoc

--- core/src/com/unibo/model/LevelsList.java
+++ core/src/com/unibo/model/LevelsList.java
@@ -1,11 +1,12 @@
 package com.unibo.model;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 
- * Class that menages the levels.
+ * Class that manages game levels.
  *
  */
 public final class LevelsList {
@@ -21,19 +22,43 @@ public final class LevelsList {
         }
     }
 
+    /**
+     * 
+     * @return a list containing all the levels
+     */
+    public List<Level> getLevels() {
+        return Collections.unmodifiableList(this.levels);
+    }
+
+    /**
+     * 
+     * @return the current level
+     */
     public Level getCurrentLevel() {
         return this.levels.get(this.counter);
     }
 
+    /**
+     * 
+     * @return true if there is at least another level
+     */
     public boolean hasNextLevel() {
         return this.levels.size() > this.counter;
     }
 
+    /**
+     * 
+     * @return the next level
+     */
     public Level getNextLevel() {
         this.counter++;
         return this.levels.get(this.counter);
     }
 
+    /**
+     * 
+     * @return true if there are no more levels
+     */
     public boolean isGameOver() {
         return !this.hasNextLevel();
     }
