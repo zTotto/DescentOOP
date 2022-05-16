2cff6fe5fcaed7dbe145f872229a36958e6ad3df
Lorenzo Todisco <49115537+zTotto@users.noreply.github.com>
2022-05-16 13:21:51 +0200

    Minor formatting fixes

--- core/src/com/unibo/keybindings/KeyBindings.java
+++ core/src/com/unibo/keybindings/KeyBindings.java
@@ -44,15 +44,15 @@ public enum KeyBindings {
      */
     USE_POTION(Input.Keys.F, "UsePotion"),
     /**
-     * Key to use skill: Increases Speed
+     * Key to use skill: Increases Speed.
      */
     INCREASES_SPEED(Input.Keys.SHIFT_LEFT, "HoldIncreasesSpeed"),
     /**
-     * Key to use skill: Heal
+     * Key to use skill: Heal.
      */
     HEAL(Input.Keys.A, "Heal"),
     /**
-     * Key to open the skill menù
+     * Key to open the skill menu.
      */
     SKILL_MENU(Input.Keys.T, "SkillMenu");
 
