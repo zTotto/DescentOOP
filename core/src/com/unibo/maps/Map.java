ea3ebcb2e94fa9b54c1df749d7b6183261f8b686
Lorenzo Todisco <49115537+zTotto@users.noreply.github.com>
2022-05-20 16:59:22 +0200

    Minor fix

--- core/src/com/unibo/maps/Map.java
+++ core/src/com/unibo/maps/Map.java
@@ -13,19 +13,15 @@ import com.unibo.util.Position;
 public interface Map {
     /**
      * Method to check whether a movement is valid.
+     * 
      * @param charView view of the character
-     * @param newX x coordinate to test
-     * @param newY y coordinate to test
+     * @param newX     x coordinate to test
+     * @param newY     y coordinate to test
      * @return true if the movement is valid.
      */
-    //boolean validMovement(Character character, Direction dir);
     Boolean validMovement(CharacterView charView, int newX, int newY);
-    
-    Boolean checkTeleport(final CharacterView charView);
 
     void addItem(Item item, Position pos); // come segnalare posizione invalida?
-
-    // void addItem(Item item);
 
     Position getStartingPosition();
 
