514b353b47a190efd26001199f381df6915aedd8
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2022-06-24 11:32:50 +0200

    Add ExpToLevelUp when creating the hero and some minor fixes

--- core/src/com/unibo/game/GameScreen.java
+++ core/src/com/unibo/game/GameScreen.java
@@ -52,6 +52,7 @@ public class GameScreen implements Screen {
     private static final int MAX_SPEED = 200;
     private static final int MAX_HP = 100;
     private static final int MAX_MANA = 100;
+    private static final long EXP_TO_LVL_UP = 60;
     private static final double SPEED_MULTIPLIER = 0.75;
     private static final double TEN_PERCENT_MULTIPIER = 0.2;
     private final Descent game;
@@ -152,7 +153,7 @@ public class GameScreen implements Screen {
         Texture bloodTexture = new Texture("characters/bloodMob.png");
         bloodAnim = TextureRegion.split(bloodTexture, bloodTexture.getWidth() / 12, bloodTexture.getHeight())[0];
 
-        heroView = new HeroView(new Hero("Ross", MAX_HP, MAX_SPEED, MAX_MANA), this.input, audioManager);
+        heroView = new HeroView(new Hero("Ross", MAX_HP, MAX_SPEED, MAX_MANA, EXP_TO_LVL_UP), this.input, audioManager);
         lvlView.setHeroView(heroView);
         this.skillMenu = new SkillMenu(this, heroView.getCharacter());
         this.skillMenu.getMenu().setVisible(true);
